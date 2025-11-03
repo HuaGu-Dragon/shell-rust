3dcd8db2ccabffccf4d4befb7742e9e4a2018f91
俺不叫化咕龙 <1801943622@qq.com>
2025-11-03 15:08:49 +0800

    Fix mplement a REPL.

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use anyhow::Context;
 
 fn main() -> anyhow::Result<()> {
     // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
 
     let mut buf = String::new();
     loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
         io::stdin()
             .read_line(&mut buf)
             .context("read user input into buf")?;
