ad358fa6f1308b6437db322c19d1bbe09a3f83d3
俺不叫化咕龙 <1801943622@qq.com>
2025-11-03 15:05:43 +0800

    Handle invalid commands.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn main() {
+use anyhow::Context;
+
+fn main() -> anyhow::Result<()> {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut buf = String::new();
+    let input = io::stdin()
+        .read_line(&mut buf)
+        .context("read user input into buf")?;
+
+    let com = buf.trim();
+    println!("{com}: command not found");
+
+    Ok(())
 }
