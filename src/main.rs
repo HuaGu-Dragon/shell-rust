a4d709147419bdcfeffdd0e8ae220cd4e98a7e29
俺不叫化咕龙 <1801943622@qq.com>
2026-05-08 14:12:23 +0800

    Expanding empty variables

--- src/main.rs
+++ src/main.rs
@@ -645,61 +645,67 @@ impl Iterator for &mut Parser<'_, '_> {
     type Item = String;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let mut next = self.shlex.next()?;
+        loop {
+            let mut next = self.shlex.next()?;
 
-        // TODO: Handle error
-        if let Some(idx) = next.find("$") {
-            let mut result = String::from(&next[..idx]);
-            let mats = next[idx.saturating_add(1)..].split("$");
-            for mut mat in mats {
-                if mat.starts_with('{') {
-                    mat = &mat[1..];
-                    if let Some(end) = mat.find('}') {
-                        if let Some(value) = self.decls.get(&mat[..end]) {
-                            result.push_str(value);
+            // TODO: Handle error
+            if let Some(idx) = next.find("$") {
+                let mut result = String::from(&next[..idx]);
+                let mats = next[idx.saturating_add(1)..].split("$");
+                for mut mat in mats {
+                    if mat.starts_with('{') {
+                        mat = &mat[1..];
+                        if let Some(end) = mat.find('}') {
+                            if let Some(value) = self.decls.get(&mat[..end]) {
+                                result.push_str(value);
+                            }
+                            result.push_str(&mat[end.saturating_add(1)..]);
+                            continue;
                         }
-                        result.push_str(&mat[end.saturating_add(1)..]);
-                        continue;
+                    }
+
+                    if let Some(value) = self.decls.get(mat) {
+                        result.push_str(value);
                     }
                 }
 
-                if let Some(value) = self.decls.get(mat) {
-                    result.push_str(value);
-                }
+                next = result;
+            } else if next == ">" || next == "1>" {
+                self.stdout = Some(File::create(self.shlex.next()?).unwrap());
+                next = self.shlex.next()?;
+            } else if next == "2>" {
+                self.stderr = Some(File::create(self.shlex.next()?).unwrap());
+                next = self.shlex.next()?;
+            } else if next == ">>" || next == "1>>" {
+                self.stdout = Some(
+                    File::options()
+                        .append(true)
+                        .create(true)
+                        .open(self.shlex.next()?)
+                        .unwrap(),
+                );
+                next = self.shlex.next()?;
+            } else if next == "2>>" {
+                self.stderr = Some(
+                    File::options()
+                        .append(true)
+                        .create(true)
+                        .open(self.shlex.next()?)
+                        .unwrap(),
+                );
+                next = self.shlex.next()?;
+            } else if next == "&" {
+                // TODO: check if & is the last token
+                self.background = true;
+                next = self.shlex.next()?;
             }
 
-            next = result;
-        } else if next == ">" || next == "1>" {
-            self.stdout = Some(File::create(self.shlex.next()?).unwrap());
-            next = self.shlex.next()?;
-        } else if next == "2>" {
-            self.stderr = Some(File::create(self.shlex.next()?).unwrap());
-            next = self.shlex.next()?;
-        } else if next == ">>" || next == "1>>" {
-            self.stdout = Some(
-                File::options()
-                    .append(true)
-                    .create(true)
-                    .open(self.shlex.next()?)
-                    .unwrap(),
-            );
-            next = self.shlex.next()?;
-        } else if next == "2>>" {
-            self.stderr = Some(
-                File::options()
-                    .append(true)
-                    .create(true)
-                    .open(self.shlex.next()?)
-                    .unwrap(),
-            );
-            next = self.shlex.next()?;
-        } else if next == "&" {
-            // TODO: check if & is the last token
-            self.background = true;
-            next = self.shlex.next()?;
+            if next.is_empty() {
+                continue;
+            } else {
+                break Some(next);
+            }
         }
-
-        Some(next)
     }
 }
 
